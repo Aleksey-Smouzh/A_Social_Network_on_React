1b78dd89e9a308e25b09fec2e517fc0086de22dd
Aleksey-Smouzh <xmz@tut.by>
2022-02-25 22:51:59 +0300

    add image, style components

--- sn_on_react/src/components/Header/Header.jsx
+++ sn_on_react/src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './Header.css';
+import logo from '../Image/logo.svg';
+
 function Header() {
   return (
-    <div className='container_header'>header</div>
+    <div className='container_header'>header
+    
+    <img className='logo' src={logo} alt="cherry" />
+    
+    
+    
+    
+    </div>
   )
 }
 
