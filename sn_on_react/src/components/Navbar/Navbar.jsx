80cb2cbede5840f7688014f9b3a2767e8b4b9a99
Aleksey-Smouzh <xmz@tut.by>
2022-02-25 23:48:07 +0300

    add Navbar.css

--- sn_on_react/src/components/Navbar/Navbar.jsx
+++ sn_on_react/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,19 @@ import './Navbar.css';
 
 function Navbar() {
   return (
-    <div className='container_navbar'>Navbar</div>
+    <div className='container_navbar'>
+    
+    <nav className='navbar_nav'>
+        <ul className='navbar_ul'>
+        <li className='navbar_list'>Your profile</li>
+        <li className='navbar_list'>Messages</li>
+        <li className='navbar_list'>News</li>
+        <li className='navbar_list'>Music</li>
+        <li className='navbar_list'>Settings</li>
+        </ul>
+    </nav>
+    
+    </div>
   )
 }
 
