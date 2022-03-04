91011a7afc4d9a17237755a44ffe33d11a577483
Aleksey-Smouzh <xmz@tut.by>
2022-03-04 10:44:36 +0300

    resumption 4.03.22

--- sn_on_react/src/components/Navbar/Navbar.jsx
+++ sn_on_react/src/components/Navbar/Navbar.jsx
@@ -6,17 +6,17 @@ import './Navbar.css';
 function Navbar() {
   return (
     <div className='container_navbar'>
-    
-    <nav className='navbar_nav'>
-        <ul className='navbar_ul'>
-        <li className='navbar_list'>Your profile</li>
-        <li className='navbar_list'>Messages</li>
-        <li className='navbar_list'>News</li>
-        <li className='navbar_list'>Music</li>
-        <li className='navbar_list'>Settings</li>
-        </ul>
-    </nav>
-    
+
+          <nav className='navbar_nav'>
+              <ul className='navbar_ul'>
+                  <li className='navbar_list'> <a href="/s">Your profile</a></li>
+                  <li className='navbar_list'>  <a href="/s">Messages</a></li>
+                  <li className='navbar_list'>  <a href="/s">News</a></li>
+                  <li className='navbar_list'>  <a href="/s">Music</a></li>
+                  <li className='navbar_list'>  <a href="/s">Settings</a></li>
+              </ul>
+          </nav>
+
     </div>
   )
 }
--- sn_on_react/src/components/Profile/Profile.jsx
+++ sn_on_react/src/components/Profile/Profile.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './Profile.css';
-
+import avatar from '../Image/avatar.jpeg';
 
 
 
 function Profile() {
   return (
-    <div className='container_profile'>Profile</div>
+    <div className='container_profile'>Profile
+    
+    <img className='profile_avatar' src={avatar} alt="avatar" />
+      
+      <div className='profile_description'>
+          <h2 className="description_name_nickname">Name</h2>
+          
+      </div>
+     
+    </div>
   )
 }
 
