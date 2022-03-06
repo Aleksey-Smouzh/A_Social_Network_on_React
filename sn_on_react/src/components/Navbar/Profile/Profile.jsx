d08042464fe3f6a63a0081173c20461bed9ed1d5
Aleksey-Smouzh <xmz@tut.by>
2022-03-06 21:35:59 +0300

    map Profile

--- sn_on_react/src/components/Navbar/Profile/Profile.jsx
+++ sn_on_react/src/components/Navbar/Profile/Profile.jsx
@@ -12,29 +12,26 @@ return(
 }
 
 
-
-
-
 function Profile(props) {
 
 let profileData = [
-{id:1, name: "Pupkin", age: "23", country: "Gonduras ",city: "Chupacabra"},
-{id:2, name: "Gonza", age: "34", country: "Gorza",city: "Chupxondric"},
-{id:3, name: "Krukva", age: "73", country: "Nikaragua ",city: "Minskas"},
-{id:4, name: "Pronya", age: "13", country: "Brazilia ",city: "Prazenbam"},
-{id:5, name: "Patrik", age: "63", country: "Yotube",city: "Culima"},
+{id:1, name: "Pupkin", age: "23", country: "Gonduras ", city: "Chupacabra"},
+// {id:2, name: "Gonza", age: "34", country: "Gorza",city: "Chupxondric"},
+// {id:3, name: "Krukva", age: "73", country: "Nikaragua ",city: "Minskas"},
+// {id:4, name: "Pronya", age: "13", country: "Brazilia ",city: "Prazenbam"},
+// {id:5, name: "Patrik", age: "63", country: "Yotube",city: "Culima"},
 ]
 
-
-
+let profileItems = profileData.map(p => <ProfileName name={p.name} age={p.age} country={p.country} city={p.city}/>)
+                        // почему то не работает все элементы кроме name
   return (
     <div className={profile.container}>Profile
     
     <img className={profile.avatar} src={avatar} alt="avatar" />
       
       <div className={profile.description}>
-          
-          <ProfileName name={profileData[0].name} age={profileData[0].age}/>
+          {profileItems}
+         
       </div>
      <MyPosts/>
 
