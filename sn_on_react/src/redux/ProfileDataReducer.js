59394c8423b1a82274ec27938a5aa043e37b07b5
Aleksey-Smouzh <xmz@tut.by>
2022-03-25 19:25:30 +0300

    fixed ProfileData Reduser

--- sn_on_react/src/redux/ProfileDataReducer.js
+++ sn_on_react/src/redux/ProfileDataReducer.js
@@ -1,6 +1,20 @@
 
 
-let initialState = {}
+let initialState = [
+    
+        {
+            id: 1,
+            name: "Pupkin",
+            age: "23",
+            country: "Gonduras ",
+            city: "Chupacabra",
+        }
+        // {id:2, name: "Gonza", age: "34", country: "Gorza",city: "Chupxondric"},
+        // {id:3, name: "Krukva", age: "73", country: "Nikaragua ",city: "Minskas"},
+        // {id:4, name: "Pronya", age: "13", country: "Brazilia ",city: "Prazenbam"},
+        // {id:5, name: "Patrik", age: "63", country: "Yotube",city: "Culima"},
+    
+]
 
 const ProfileDataReducer = (state = initialState, action) => {
     return (
