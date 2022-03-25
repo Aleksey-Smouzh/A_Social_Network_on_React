ffa03dbf8afba9346c8232d8066323bbe279f004
Aleksey-Smouzh <xmz@tut.by>
2022-03-25 22:19:35 +0300

    fixed dispatch index.js

--- sn_on_react/src/index.js
+++ sn_on_react/src/index.js
@@ -10,9 +10,9 @@ let rerenderEntireTree = (state) => {
     <React.StrictMode>
       <App
         state={state} 
-        //dispatch={store.dispatch.bind(store)}  
+        dispatch={store.dispatch.bind(store)}  
         store={store}
-           //addMessage={store.addMessage.bind(store)}
+          //  addMessage={store.addMessage.bind(store)}
 
       />
     </React.StrictMode>,
