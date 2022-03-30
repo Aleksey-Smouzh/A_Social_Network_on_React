66dcf15a00cdeb99144ed8bdc3a5779869ee717d
Aleksey-Smouzh <xmz@tut.by>
2022-03-30 15:39:29 +0300

    added Store-context to index.js

--- sn_on_react/src/index.js
+++ sn_on_react/src/index.js
@@ -14,9 +14,9 @@ let rerenderEntireTree = (state) => {
       <BrowserRouter>
       <StoreContext.Provider value={store}>
       <App
-        state={state} 
-        dispatch={store.dispatch.bind(store)}  
-        store={store}
+        // state={state} 
+        // dispatch={store.dispatch.bind(store)}  
+        // store={store}
           //  addMessage={store.addMessage.bind(store)}
 
       />
